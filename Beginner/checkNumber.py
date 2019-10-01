ab83cc2728c82861966d25d564f85df6e072d609
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2019-10-01 15:18:23 -0300

    Add checkNumber and checkAge - Beginner

---
+++ Beginner/checkNumber.py
@@ -0,0 +1,7 @@
+#CInput The Age, if age>18 print adult , if 10heck Whether Number is Odd or Not
+
+number = int(input("Which number would you like to check?"))
+if number % 2 == 0:
+    print(f"The {number} is Even.")
+else:
+    print(f"The {number} is Odd.")
