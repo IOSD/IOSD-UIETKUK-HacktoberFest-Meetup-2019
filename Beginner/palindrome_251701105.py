f1aa3b209d4a17679693c3d1fd5c94e0c31763d5
Ankur <34418414+Ankuraxz@users.noreply.github.com>
2019-10-07 12:08:46 +0530

    palindrome_251701105.py
    Palindrome check by Ankur-251701105

---
+++ Beginner/palindrome_251701105.py
@@ -0,0 +1,9 @@
+# Check if string is palindrom or not....using in-built fxn
+
+a = str(input("Input String"))
+b = a.reverse
+
+if a==b:
+  print("string is palindrome")
+else:
+  print("String is not palindrome")
