7ef3933e6cc3e6fa1f07772aab9f697986d5bd9f
yogesh-1952 <50928637+yogesh-1952@users.noreply.github.com>
2019-10-07 13:24:57 +0530

    odd_even_40815002718.cpp

---
+++ Beginner/odd_even_40815002718.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"Enter any number<<endl;
+  cin>>n;
+  if(n%2==0)
+    cout<<"Even Number"<<endl;
+  else
+    cout<<"Odd Number"<<endl;
+  return ;
+}
