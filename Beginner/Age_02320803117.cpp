fab3db00d9074c1f71e8d2f14be34ebdfd2fd3cd
Varun <37479544+varunrocxx@users.noreply.github.com>
2019-10-07 19:54:43 +0530

    Age_02320803117.cpp
    Age Finder

---
+++ Beginner/Age_02320803117.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n ;
+    cout << "Input age";
+    cin >> n;
+    if(n>=18)
+        cout << "Adult ";
+    else 
+        cout<<"Not an adult";
+return 0;
+}
