ee8e206aaed620d8c1f05f5b80ceaaec26a4b7ff
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2019-10-07 13:28:09 +0530

    agecheck.cpp

---
+++ Beginner/agecheck.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int age ;
+		//input age
+    
+	cout<<"Enter the age of the person: ";
+ cin>>age;
+
+	if (age>=18)
+	{
+		cout<<"Adult;
+	}
+	else
+	{
+		cout<<"Not an adult";
+	}	
+
+	return 0;
+}
