b9b17edb7eb9f53cad1694c6a66a8f4dbc4b22c1
= <abhaychauhan@ABHAYs-MacBook-Air.local>
2019-10-07 22:53:12 +0530

    cpp pattern program

---
+++ Beginner/pattern.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int i, j, n, k = 0;
+
+    cout << "\n Enter number of rows: ";
+    cin >> n;
+
+    for (i = 1, k = 0; i <= n; i++, k = 0)
+    {
+        for (j = 1; j <= (n - i); j++)
+        {
+            cout << " ";
+        }
+        while (k != (2 * i - 1))
+        {
+            cout << "*";
+            k++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
