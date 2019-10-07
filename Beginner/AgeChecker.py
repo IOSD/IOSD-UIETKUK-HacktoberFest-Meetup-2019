0502eb52f264f3d6fe4d861c014a7fae42546be5
omkardiwan922 <35604011+omkardiwan@users.noreply.github.com>
2019-10-07 13:14:27 +0530

    Agechecker.py
    TO check age

---
+++ Beginner/AgeChecker.py
@@ -0,0 +1,7 @@
+Age = int(input("Enter the age to find weather child,teen or adult\n"))    #To find weather child,teen,adult
+if(Age <10):
+    print("You are Child")
+elif(10<Age<=18):
+    print("You are Teen")
+else:
+    print("You are adult")
