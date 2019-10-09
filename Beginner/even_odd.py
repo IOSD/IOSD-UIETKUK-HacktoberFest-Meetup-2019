d39fb967b16d300833c95be80cad4994bb3fa009
Branden Morgan <branden.morgan@outlook.com>
2019-10-09 09:56:28 -0400

    create even_odd.py
    Even Odd beginner challenge

---
+++ Beginner/even_odd.py
@@ -0,0 +1,13 @@
+# This program will check to see if a number is even or odd.
+# Any number = n
+# 2n = even
+# 2n-1 = odd
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+
+# Author Branden Morgan
+# Thanks Google
