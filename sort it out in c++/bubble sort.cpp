41cee3e2346fb6389d1b022d71b6ade47e32af81
SIDHANT2001 <56162529+SIDHANT2001@users.noreply.github.com>
2019-10-10 19:11:01 +0530

    added "bubble sort.cpp" by sidhant2001
    It explains the concept of bubble sort thoroughly through code.
    Read it out to explore more about sorting!

---
+++ sort it out in c++/bubble sort.cpp
@@ -0,0 +1,27 @@
+#include<iostream.h>
+#include<conio.h> 
+void main()
+{
+clrscr();
+	int a[50],n,i,j,temp;
+	cout<<"Enter the size of array: ";
+	cin>>n;
+	cout<<"Enter the array elements: "; 
+	for(i=0;i<n;i++)
+		cin>>a[i];
+		
+	for(i=1;i<(n-1);++i)
+	{
+		for(j=0;j<(n-i);++j)
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+	}
+	
+	cout<<"Array after bubble sort:\n";
+	for(i=0;i<n;++i)
+		cout<<" "<<a[i];
+}
