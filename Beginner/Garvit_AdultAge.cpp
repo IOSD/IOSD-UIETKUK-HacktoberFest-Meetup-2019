a8243177819914bdf80edd4f90c78da18603d1e0
Garvit-32 <54506526+Garvit-32@users.noreply.github.com>
2019-10-10 10:29:51 +0530

    Create Garvit_AdultAge.cpp
    Adult Age program by Garvit

---
+++ Beginner/Garvit_AdultAge.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n ;
+    cout << "Input age";
+    cin >> n;
+    if(n>=18)
+        cout << "Adult ";
+    else 
+        cout<<"Not an adult";
+return 0;
+}
