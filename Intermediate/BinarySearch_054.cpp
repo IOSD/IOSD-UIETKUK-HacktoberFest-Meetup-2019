6aacba01e00328a56d8dbf7eb904404bc4094d9a
Jaskirat singh <44332209+singhjaskirat984@users.noreply.github.com>
2019-10-11 21:13:17 +0530

    Update BinarySearch_054.cpp

--- Intermediate/BinarySearch_054.cpp
+++ Intermediate/BinarySearch_054.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-	int i,j,arr[100],temp,size; 
+	int i,j,arr[100],temp,size,n,l,h,mid,flag=0,pos; 
 	cout<<"enter the size of the array\n";
 	cin>>size;
 	cout<<"\nenter the array";
@@ -12,20 +12,36 @@ int main() {
 		cin>>arr[i];
 		}
 		
-	for(i=0;i<size;i++){
-		for(j=0;j<size-i-1;j++){
-			if(arr[j] > arr[j+1]){
-				temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
-			}
+	cout<<"enter the no. to search";
+	cin>>n;
+	
+	l=0;
+	h=size-1;
+
+	while(l<=h){
+		mid = (l+h)/2;
+		if(n==arr[mid]){
+			flag = 1;
+			pos = mid;
+			break;
+		}
+		
+		else if(n>arr[mid]){
+			l = mid+1;
+		}
+		
+		else if(n<arr[mid]){
+			h = mid-1;
 		}
 	}
 	
-	cout<<"sorted array is-";
-	for(i=0;i<size;i++){
-		cout<<arr[i]<<"\n";
+	if(flag==1){
+		cout<<"element found at position"<<pos+1;
 	}
+	else{
+		cout<<"element not found";
+	}
+	
 	system("PAUSE");
 	getch();
 	return 0;
