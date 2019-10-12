c28a8be45f1f41fffe867067b0e79d3320971695
unknown <you@example.com>
2019-10-12 23:21:45 +0530

    created a age checker

---
+++ BEGINNER/Check_Age_prgrm.py
@@ -0,0 +1,9 @@
+#check the Age of a Person
+#This progam checks the age 
+age = int(input("Enter the Age: "))
+if(age>18):
+    print("The Person is an Adult")
+elif(age>10):
+    print("The Person is a Teen")
+else:
+    print("The Person is a Child")
