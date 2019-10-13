2694c653af5e7af86558a4d93f27f47a8a2fbbe6
sabhinandan403 <56023640+sabhinandan403@users.noreply.github.com>
2019-10-13 20:57:08 +0530

    Create evenodd.cpp

---
+++ BEGINNER/evenodd.cpp
@@ -0,0 +1,15 @@
+/even or odd
+#include<stdio.h>
+main(){
+	int n;
+	printf(" enter number ");
+	scanf("%d",&n);
+	if(n%2==0){
+		printf(" number is even");
+		
+		
+	}
+	else{
+		printf(" number is odd");
+		
+	}
