d74e7f159704660ef304b5566f7875d65c9f0fa5
RaDaDiYaMoHiT <37595144+RaDaDiYaMoHiT@users.noreply.github.com>
2019-10-15 08:55:58 +0530

    Create RaDaDiYaMoHiT.py
    program to implement Input The Age, if age > 18 print adult , if 10 < age <= 18 print teen, if age < 10 print child.

---
+++ RaDaDiYaMoHiT.py
@@ -0,0 +1,18 @@
+#Write a program to implement Input The Age, if age > 18 print adult , if 10 < age <= 18 print teen, if age < 10 print child.
+
+#Start the program below...
+
+age=int(input())
+if(age>18):
+  print("Adult")
+elif(age>10 and age<=18):
+  print("teen")
+elif(age<10):
+  print("child")
+  
+###
+-Name:Mohit Radadiya
+-Location: Surat,Gujarat
+-Bio: Web Developer
+-Github://github.com/RaDaDiYaMoHiT
+***
