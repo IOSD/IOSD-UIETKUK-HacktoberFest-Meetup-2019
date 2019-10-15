3c2038785d3381610e1d4af7fbea6b7e6a082323
jscoeiot <56603573+jscoeiot@users.noreply.github.com>
2019-10-15 22:51:57 +0530

    Create SumofNaturalNumbers.py

---
+++ SumofNaturalNumbers.py
@@ -0,0 +1,14 @@
+num = 16
+
+# uncomment to take input from the user
+#num = int(input("Enter a number: "))
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate un till zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is",sum)
