d4e4442742fc36911dea37d60de833a26857f153
Neerajpardeshi <54471310+Neerajpardeshi@users.noreply.github.com>
2019-10-16 11:34:47 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+public static void main(String[] args) 
+	{
+		    int n, a = 0, b = 0, c = 1;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter value of n:");
+        n = s.nextInt();
+        System.out.print("Fibonacci Series:");
+        for(int i = 1; i <= n; i++)
+        {
+            a = b;
+            b = c;
+            c = a + b;
+            System.out.print(a+" ");
+        }
+	}
