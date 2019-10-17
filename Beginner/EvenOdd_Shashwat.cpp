3c4dd26d7d0d13bf29eae6ef85d5288d192d24c4
Shashwat Sinha <shashwat3770@gmail.com>
2019-10-17 13:07:43 +0530

    EvenOdd_Shashwat.cpp
    even odd using bitwise

---
+++ Beginner/EvenOdd_Shashwat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+bool isEven(int n)
+{
+	return (!(n & 1));
+}
+
+int main()
+{
+	int n;
+  cin>>n;
+	isEven(n) ? cout << "Even" : cout << "Odd";
+	return 0;
+}
