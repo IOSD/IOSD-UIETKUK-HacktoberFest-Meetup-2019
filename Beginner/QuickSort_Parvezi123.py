0ca4926fb27cef22a7da73a4ce105a863094bb76
Parvezi123 <39627199+Parvezi123@users.noreply.github.com>
2019-10-20 11:14:55 +0530

    Create QuickSort_Parvezi123.py

---
+++ Beginner/QuickSort_Parvezi123.py
@@ -0,0 +1,21 @@
+def sort(array=[12,4,5,6,7,3,1,15]):
+    """Sort the array by using quicksort."""
+
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        # Don't forget to return something!
+        return sort(less)+equal+sort(greater)  # Just use the + operator to join lists
+    # Note that you want equal ^^^^^ not pivot
+    else:  # You need to handle the part at the end of the recursion - when you only have one element in your array, just return the array.
+        return array
