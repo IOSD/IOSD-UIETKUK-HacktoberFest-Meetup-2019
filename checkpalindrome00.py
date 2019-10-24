0915835a051a8fe1561f8a4664affba50144e4c5
b19149 <56934027+b19149@users.noreply.github.com>
2019-10-25 00:29:02 +0530

    checkpalindrome00.py
    checks palindrome

---
+++ checkpalindrome00.py
@@ -0,0 +1,15 @@
+def reverse(s): 
+    return s[::-1] 
+  
+def isPalindrome(s):  
+    rev = reverse(s)  
+    if (s == rev): 
+        return True
+    return False
+ 
+s = "malayalam"
+ans = isPalindrome(s) 
+if ans == 1: 
+    print("Yes") 
+else: 
+    print("No") 
