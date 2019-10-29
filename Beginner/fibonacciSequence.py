c02964c53ebcdbd36b354c3b4eb0acd7e3d2a1bc
Eric <ephan627@gmail.com>
2019-10-29 15:45:34 -0700

    Added beginner challenges in Python

--- Beginner/fibonacciSequence.py
+++ Beginner/fibonacciSequence.py
@@ -1,29 +1,8 @@
-########################## Code By - ORASHAR  ###########################
-########################## Fibonacci Sequence ###########################
-
-def Fibonacci(n):
-    n1 = 0
-    n2 = 1
-    c = 0
-    ans = []
-    if n == 1:
-        ans = n1
+def fib(num):
+    if num < 2:
+        return num
     else:
-        while c < n:
-            ans.append(n1)
-            nth = n1 + n2
-            n1 = n2
-            n2 = nth
-            c += 1
-    print(f"fibonacci sequence upto {n} terms ", end=': ')
-    for a in ans:
-        print(a, end = ' ')
+        return fib(num-1) + fib(num-2)
 
-try:
-    n = int(input("enter number of terms: "))
-    if n < 0:
-        print("Input must be a positive number")
-    else:
-        Fibonacci(n)
-except:
-    print("invalid input!")
+
+print(fib(5))
