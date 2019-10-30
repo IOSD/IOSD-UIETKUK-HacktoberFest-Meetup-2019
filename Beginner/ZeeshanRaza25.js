145fb81a61dcc86b7cc0ca380153e89c3d0ae947
Zeeshan Raza <42145844+ZeeshanRaza25@users.noreply.github.com>
2019-10-30 14:18:05 +0500

    ZeeshanRaza25.js
    Check whether Number is Odd or Even by ZeeshanRaza25

---
+++ Beginner/ZeeshanRaza25.js
@@ -0,0 +1,9 @@
+function fun(num) {
+      if (num % 2 === 0) {
+        return console.log(num + " is Not Odd");
+      } else {
+        return console.log(num + " is Odd");
+      }
+    }
+
+fun(7);
