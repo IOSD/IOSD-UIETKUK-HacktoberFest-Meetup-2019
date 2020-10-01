f21075ec72233cb8aa1d1fcc62835d230f7ed2ed
KRISHNA DEV K <57306836+scubekraise21@users.noreply.github.com>
2020-10-01 10:23:33 +0530

    ODDorEVEN.cpp

---
+++ Beginner/ODDorEVEN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   if(n%2!=0)
+   cout<<"The number is odd.";
+   else
+   cout<<"The number is even";
+   
+   return 0;
+}
