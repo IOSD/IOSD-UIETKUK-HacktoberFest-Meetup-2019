0203505ffae696acc1f3dc164f69de51c43dfbea
Devanshu Sekhar Preetam <56449787+iamdsp9@users.noreply.github.com>
2020-10-01 11:42:51 +0700

    Create checkeven.cpp

---
+++ Beginner/checkeven.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int chk;
+    cin>>chk;
+    if(chk%2==0)
+    cout<<chk " is even"<<endl;
+    else 
+    cout<<chk " is odd"<<endl;
+   }
+   return 0;
+ }
+    
