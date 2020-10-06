ab01948dc8215e42cc01b3a5a52381480c6fbe7c
KeshavSharma29 <56887924+KeshavSharma29@users.noreply.github.com>
2020-10-07 01:08:03 +0530

    Update bubblesort.c

--- Intermediate/bubblesort.c
+++ Intermediate/bubblesort.c
@@ -8,7 +8,7 @@ int main()
   printf("Enter number of elements\n");
   scanf("%d", &n);
 
-  printf("Enter %d integers\n", n);
+  printf("Enter %d integers \n", n);
 
   for (c = 0; c < n; c++)
     scanf("%d", &array[c]);
@@ -26,7 +26,7 @@ int main()
     }
   }
 
-  printf("Sorted list in ascending order:\n");
+  printf("Sorted list in ascending order :\n");
 
   for (c = 0; c < n; c++)
      printf("%d\n", array[c]);
