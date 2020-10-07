2e60e2c86af66e3b20c939f87c52903c389429b2
Yash Sharma <65597694+Mr-Yash-Sharma@users.noreply.github.com>
2020-10-07 19:18:19 +0530

    Created Check_Even_or_Odd.cpp
    C++ program to determine if a number is odd or even.

---
+++ Beginner/Check_Even_or_Odd.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+  int n;
+  cin>>n;
+  
+  if(n % 2 == 0)
+    cout<<"The number is even.";
+  else
+    cout<<"The number is odd.";
+    
+  return 0;
+}
