9b82992c21d0d686d78591cd8c75d804e56722a1
Sejal Rathore <61845736+sejal1126@users.noreply.github.com>
2020-10-18 02:30:02 +0530

    Create oddevenbysejal1126.cpp
    This is a program which will find whether the number is odd or even

---
+++ Beginner/oddevenbysejal1126.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=0;
+	cout<<"Program to find whether the number is odd or even : ";
+	cin>>n;
+	if(n%2==0)
+	{cout<<"Even number"<<endl;}
+	else
+	{cout<<"Odd number"<<endl;}
+	return 0;
+}
