ecbbdb06bdf288807592fb51d1a0d39490525f09
Sejal Rathore <61845736+sejal1126@users.noreply.github.com>
2020-10-18 04:08:15 +0530

    Create agechecksejal1126.cpp
    This program is to check age

---
+++ Beginner/agechecksejal1126.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int age;
+cout<<"enter yor age ";
+cin>>age;
+if(age<=10)
+{cout<<"child";}
+else if(age>10&&age<=18)
+{cout<<"TEEN";}
+else
+{cout<<"adult";}
+return 0;
+}
