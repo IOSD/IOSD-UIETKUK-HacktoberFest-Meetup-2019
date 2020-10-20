717a3a34b3189c29f9b97cc15f447aaeaa472a6a
Shubham Sharma <58502742+gitshubhamsharma@users.noreply.github.com>
2020-10-20 18:14:36 +0530

    Rotate_Array.py
    It will rotate the array or list in python

---
+++ Beginner/Rotate_Array.py
@@ -0,0 +1,21 @@
+
+def leftRotate(arr, d, n): 
+    for i in range(d): 
+        leftRotatebyOne(arr, n) 
+  
+
+def leftRotatebyOne(arr, n): 
+    temp = arr[0] 
+    for i in range(n-1): 
+        arr[i] = arr[i+1] 
+    arr[n-1] = temp 
+          
+  
+def printArray(arr,size): 
+    for i in range(size): 
+        print ("%d"% arr[i],end=" ") 
+  
+   
+arr = [1, 2, 3, 4, 5, 6, 7] 
+leftRotate(arr, 2, 7) 
+printArray(arr, 7) 
